d99f2b5d0b5e1ff1b2d42030317e9b309736896d
raghuvaran471 <raghuvaran471@gmail.com>
2020-05-26 19:43:03 +0530

    firstcommit

---
+++ src/varaibles/java.java
@@ -0,0 +1,16 @@
+package varaibles;
+
+public class java {
+	public static void main(String args[]) {
+		int x = 10;
+		float y = 2.2f;
+		double z = 11.34;
+		boolean is_true=false;
+		System.out.println("x");
+		System.out.println("y");
+		System.out.println("z");
+		System.out.println("false");
+		
+		}
+
+}
